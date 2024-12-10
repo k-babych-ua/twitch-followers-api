f2163dda0ee186f67949b376c7111c8d7d74d39d
Kostiantyn Babych <k.babich95@gmail.com>
2024-12-10 22:04:58 +0200

    Add project files.

---
+++ TwitchFollowers.API/Controllers/Overview.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TwitchFollowers.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Overview : ControllerBase
+    {
+        private IHttpClientFactory _httpClientFactory;
+
+        public Overview(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<string>> Get(string username)
+        {
+            using HttpClient client = _httpClientFactory.CreateClient();
+
+            var response = await client.GetAsync($"https://tools.2807.eu/api/getfollows/{username}");
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ TwitchFollowers.Domain/Extensions/HttpContentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace TwitchFollowers.Domain.Extensions
+{
+    public static class HttpContentExtensions
+    {
+        public static async Task<T> ReadAsJsonAsync<T>(this HttpContent content)
+        {
+            string stringContent = await content.ReadAsStringAsync();
+
+            if (string.IsNullOrWhiteSpace(stringContent))
+            {
+                return default;
+            }
+
+            return JsonConvert.DeserializeObject<T>(stringContent);
+        }
+    }
+}
