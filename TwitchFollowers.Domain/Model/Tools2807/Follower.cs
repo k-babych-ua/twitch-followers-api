34510a577ae765e5418621560e41af8d4255330c
Kostiantyn Babych <k.babich95@gmail.com>
2024-12-10 22:07:00 +0200

    overview endpoint

---
+++ TwitchFollowers.Domain/Model/Tools2807/Follower.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace TwitchFollowers.Domain.Model.Tools2807
+{
+    public class Follower
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "displayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty(PropertyName = "login")]
+        public string Login { get; set; }
+
+        [JsonProperty(PropertyName = "followedAt")]
+        public string FollowedAt { get; set; }
+    }
+}
